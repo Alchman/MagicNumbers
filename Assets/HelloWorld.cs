19c9e6f7054089b77d95035a8d32a6d5123aee76
Denis Alipov <st@fungi.lan>
2020-01-30 19:24:32 +0300

    Changed debug message

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -8,7 +8,7 @@ public class HelloWorld : MonoBehaviour
     void Start()
     {
         // комментарий
-        Debug.Log("Hello World"); 
+        Debug.Log("Bye World"); 
     }
 
     // Update is called once per frame
