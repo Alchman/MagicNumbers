91961af9b4e8cfa2d4d8caba6f7dbd6369c59f0a
Denis <alch007@gmail.com>
2020-02-13 21:19:50 +0300

    Lesson 6

---
+++ Assets/Scripts/MagicNumbersUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MagicNumbersUI : MonoBehaviour
+{
+    public int minNumber = 1;
+    public int maxNumber = 1000;
+
+    public Text textGuess;
+    public TextMeshProUGUI textCount;
+
+    int guess;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //guess = (minNumber + maxNumber) / 2;
+        guess = Random.Range(minNumber, maxNumber + 1);
+
+        //задать это значение компоенту  Text на сцене
+        textGuess.text = "Ваше число " + guess + "?";
+
+        textCount.text = "Количество ходов: " + 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
